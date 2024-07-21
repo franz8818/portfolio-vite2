03e24f94a3c8be365971405a7e2b80fd0b10d004
Franz Seidel <franz8818@gmail.com>
2024-07-21 18:57:46 -0500

    links redes sociales

--- src/aside/aside.jsx
+++ src/aside/aside.jsx
@@ -5,10 +5,10 @@ function Aside() {
   return (
     <>
       <section className='aside_user-info'>
-      
-      {/* Info de usuario */}
+
+        {/* Info de usuario */}
         <div className='user-info_general'>
-          
+
           <div className='user-info_container-image'>
             <img className='user-info_image' src='src/assets/img/1988.jpg' alt='User' />
           </div>
@@ -29,35 +29,45 @@ function Aside() {
           <ul className='links_social'>
 
             <li className='social_option'>
-              <a className='social_link' href='#'>
+              <a className='social_link'
+                href='https://github.com/franz8818'
+                target="_blank"
+                rel="noopener noreferrer">
                 <i className='social_icon fa-brands fa-github'></i>
               </a>
             </li>
 
+
             <li className='social_option'>
-              <a className='social_link' href='#'>
+              <a className='social_link'
+                href='https://linkedin.com/in/franz8818'
+                target="_blank"
+                rel="noopener noreferrer">
                 <i className='social_icon fa-brands fa-linkedin'></i>
               </a>
             </li>
 
             <li className='social_option'>
-              <a className='social_link' href='#'>
+              <a className='social_link'
+                href='https://www.instagram.com/franz8818/'
+                target="_blank"
+                rel="noopener noreferrer">
                 <i className='social_icon fa-brands fa-instagram'></i>
               </a>
             </li>
 
-          </ul> 
+          </ul>
         </div>
-        
-          {/* Botón de CV */}
-          <div className='user-info_buttons'>
-            <a className='user-info_btn' href='#'>Descargar CV</a>
+
+        {/* Botón de CV */}
+        <div className='user-info_buttons'>
+          <a className='user-info_btn' href='#'>Descargar CV</a>
 
           {/* Footer */}
           <footer className='user-info_footer'>
             &copy; 2024 Made with React ♥︎
           </footer>
-          </div>
+        </div>
 
       </section>
 
