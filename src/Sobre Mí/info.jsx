1473bb0cdd932f18846812ace1bd6335eeabe5ab
Franz Seidel <franz8818@gmail.com>
2024-07-22 12:54:11 -0500

    cuadrando los estilos generales

--- src/Sobre Mí/info.jsx
+++ src/Sobre Mí/info.jsx
@@ -6,45 +6,52 @@ function Info() {
 
   return (
     <>
-      {/* Título Sobre Mí */}
+
       <section className='content_page content_about'>
-        <header className='about_header'>
-          <h2 className='about_title'>Sobre <span className="title_color">Mí</span></h2>
-        </header>
 
         {/* Sobre Mí */}
         <section className='about_personal-info'>
 
-          <article className='personal-info_bio'>
-            <p className='personal-info_description'>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum ipsam, sed adipisci officia possimus quibusdam minus quaerat blanditiis? Ex laudantium, eligendi alias enim consectetur quae vitae odit beatae nesciunt quod?
-              Qui repellat ipsum expedita sapiente pariatur laborum voluptate dolore quas voluptates. Doloremque, vitae voluptatem earum, aliquid fuga iusto dolore suscipit maxime quaerat illo quibusdam quidem numquam, nisi deserunt incidunt voluptatibus.
-            </p>
-          </article>
+          <header className='about_header'>
+            <h2 className='about_title'>
+              Sobre <span className="title_color">Mí</span>
+            </h2>
+          </header>
 
-          <div className='personal-info_data'>
-            <ul className='personal-info_list'>
-              <li className='personal-info_option'>
-                <span className='personal-info_title'>Ubicación:</span>
-                <span className='personal-info_value'>Medellin, Colombia</span>
-              </li>
-              <li className='personal-info_option'>
-                <span className='personal-info_title'>correo:</span>
-                <span className='personal-info_value'>franz8818@gmail.com</span>
-              </li>
-              <li className='personal-info_option'>
-                <span className='personal-info_title'>Teléfono:</span>
-                <span className='personal-info_value'>+57 316 270 2385 </span>
-              </li>
-              <li className='personal-info_option'>
-                <span className='personal-info_title'>Teléfono:</span>
-                <span className='personal-info_value'>+57 316 270 2385 </span>
-              </li>
-              <li className='personal-info_option'>
-                <span className='personal-info_title'>Teléfono:</span>
-                <span className='personal-info_value'>+57 316 270 2385 </span>
-              </li>
-            </ul>
+          <div className='personal-info_container'>
+
+            <article className='personal-info_bio'>
+              <p className='personal-info_description'>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum ipsam, sed adipisci officia possimus quibusdam minus quaerat blanditiis? Ex laudantium, eligendi alias enim consectetur quae vitae odit beatae nesciunt quod?
+                Qui repellat ipsum expedita sapiente pariatur laborum voluptate dolore quas voluptates. Doloremque, vitae voluptatem earum, aliquid fuga iusto dolore suscipit maxime quaerat illo quibusdam quidem numquam, nisi deserunt incidunt voluptatibus.
+              </p>
+            </article>
+
+            <div className='personal-info_data'>
+              <ul className='personal-info_list'>
+                <li className='personal-info_option'>
+                  <span className='personal-info_title'>Ubicación:</span>
+                  <span className='personal-info_value'>Medellin, Colombia</span>
+                </li>
+                <li className='personal-info_option'>
+                  <span className='personal-info_title'>correo:</span>
+                  <span className='personal-info_value'>franz8818@gmail.com</span>
+                </li>
+                <li className='personal-info_option'>
+                  <span className='personal-info_title'>Teléfono:</span>
+                  <span className='personal-info_value'>+57 316 270 2385 </span>
+                </li>
+                <li className='personal-info_option'>
+                  <span className='personal-info_title'>Teléfono:</span>
+                  <span className='personal-info_value'>+57 316 270 2385 </span>
+                </li>
+                <li className='personal-info_option'>
+                  <span className='personal-info_title'>Teléfono:</span>
+                  <span className='personal-info_value'>+57 316 270 2385 </span>
+                </li>
+              </ul>
+            </div>
+
           </div>
 
         </section>
@@ -53,12 +60,12 @@ function Info() {
         <section className='about_services'>
 
           <header className='services_header'>
-            <h3 className='services_title'>
+            <h2 className='services_title'>
               Lo que <span className="title_color">hago</span>
-            </h3>
+            </h2>
           </header>
 
-          <div className='service_container'>
+          <div className='services_container'>
 
             <article className='services_service'>
 
@@ -126,6 +133,7 @@ function Info() {
 
         {/* Información extra */}
         <section className='about_extra'>
+          
           <header className='extra_header'>
             <h2 className='extra_title'>
               Información <span className='title_color'>Extra</span>
