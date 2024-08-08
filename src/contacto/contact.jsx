6875f190d7a2ce8b305d848216dd24217b1eb60a
Franz Seidel <franz8818@gmail.com>
2024-08-08 17:04:22 -0500

    maquetando html de contact

--- src/contacto/contact.jsx
+++ src/contacto/contact.jsx
@@ -5,12 +5,85 @@ function Contact() {
 
   return (
     <>
-      {/* Sobre Mí */}
-        <section className='content_contact'>
-        <h1>
-          <div className='sobre_mi'>Contacto</div>
-        </h1>
-        </section>
+      {/* Título Contacto */}
+      <section className='content_page content_contact'>
+
+        <header className='contact_header'>
+          <h1 className='contact_title'>Contacto</h1>
+        </header>
+
+        <div className='contact_container'>
+
+          <section className='contact_info'>
+
+            <div className='contact_data'>
+              <i className='fa-solid fa-location-dot'></i>
+              <h2 className='contact_subtitle'>Colombia</h2>
+            </div>
+
+            <div className='contact_data'>
+              <i className='fa-solid fa-phone'></i>
+              <h2 className='contact_subtitle'>+57 316 270 23 85</h2>
+            </div>
+
+            <div className='contact_data'>
+              <i className='fa-solid fa-envelope'></i>
+              <h2 className='contact_subtitle'>franz8818@gmail.com</h2>
+            </div>
+
+            <div className='contact_data'>
+              <i className='fa-solid fa-circle-check'></i>
+              <h2 className='contact_subtitle'>Diseñador Front-end</h2>
+            </div>
+
+          </section>
+
+          <section className='contact_form-box'>
+
+            <div className='contact_map'></div>
+            
+            <header className='contact_form-header'>
+              <h3 className='contact_subtitle'>¡Trabajemos <span className='subtitle_color'>juntos!</span> </h3>
+            </header>
+
+            <form className='contact_form'></form>
+
+            <div className='form_container'>
+
+              <div className='form_left'>
+
+                <div className='form_group'>
+                  <input type='text' className='form_input' name='name' required placeholder='Nombre'></input>
+                  <label for="name" className='form_label'>Nombre</label>
+                </div>
+              </div>
+
+              <div className='form_group'>
+                <input type='text' className='form_input' name='e-mail' required placeholder='e-mail'></input>
+                <label for="name" className='form_label'>Nombre</label>
+              </div>
+
+            </div>
+
+            <div className='form_group'>
+              <input type='text' className='form_input' name='asunto' required placeholder='asunto'></input>
+              <label for="name" className='form_label'>Nombre</label>
+            </div>
+
+          </section >
+
+
+        </div>
+
+        <div className='form_right'></div>
+
+
+
+
+
+      </section >
+
+
     </>
   )
 }
