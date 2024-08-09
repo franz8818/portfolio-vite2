06f84ddc8551adc58065bfd2c0634b96fa1a54e9
Franz Seidel <franz8818@gmail.com>
2024-08-09 17:55:26 -0500

    insertamos un iframe de google maps

--- src/contacto/contact.jsx
+++ src/contacto/contact.jsx
@@ -40,7 +40,9 @@ function Contact() {
 
           <section className='contact_form-box'>
 
-            <div className='contact_map'></div>
+            <div className='contact_map'>
+            <iframe frameborder="0" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.9821968830474!2d-75.38184222470969!3d6.133094027576386!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e469f28acb535cb%3A0x82ee7b7256893f20!2sCra.%2055b%2C%20Rionegro%2C%20Antioquia!5e0!3m2!1ses!2sco!4v1723243348828!5m2!1ses!2sco" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
 
             <header className='contact_form-header'>
               <h3 className='contact_subtitle'>¡Trabajemos <span className='subtitle_color'>juntos!</span> </h3>
