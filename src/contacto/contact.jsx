5f14b7c7e016b64f01c349931156616948f98e5e
Franz Seidel <franz8818@gmail.com>
2025-02-21 18:29:55 -0500

    probando hinchada de pelotas

--- src/contacto/contact.jsx
+++ src/contacto/contact.jsx
@@ -20,8 +20,6 @@ function Contact() {
               {/* <iframe frameborder="0" marginheight="0" marignwidth="0" loading="lazy" referrerpolicy="no-referrer-when-downgrade" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.9821968830474!2d-75.38184222470969!3d6.133094027576386!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e469f28acb535cb%3A0x82ee7b7256893f20!2sCra.%2055b%2C%20Rionegro%2C%20Antioquia!5e0!3m2!1ses!2sco!4v1723243348828!5m2!1ses!2sco"></iframe> */}
             </div>
 
-
-
             <form className='contact_form'>
 
               <div className='form_container'>
@@ -34,7 +32,7 @@ function Contact() {
                   </div>
 
                   <div className='form_group'>
-                    <input type='e-mail' className='form_input' name='e-mail' required placeholder='email'></input>
+                    <input type='e-mail' className='form_input' name='e-mail' required placeholder='e-mail'></input>
                     <label for="email" className='form_label'>e-mail</label>
                   </div>
 
@@ -43,18 +41,14 @@ function Contact() {
                     <label for="subject" className='form_label'>Asunto</label>
                   </div>
 
+                  <div className='form_group form_group--textarea'>
+                    <textarea className='form_input form_input--textarea' name='message' required placeholder='Mensaje'></textarea>
+                    <label for="message" className='form_label'></label>
+                  </div>
+
                 </section>
 
               </div>
-
-              <section className='form_left'>
-
-                <div className='form_group'>
-                  <textarea className='form_input form_input--textarea' name='message' required placeholder='Mensaje'></textarea>
-                  <label for="message" className='form_label'></label>
-                </div>
-
-              </section>
 
               <input type='submit' className='form_button' value="Enviar Mensaje"></input>
 
